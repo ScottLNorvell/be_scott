76a51d222b54ab5d30cc3855c7c1690b35287a22
Scott Norvell <scottlnorvell@gmail.com>
2013-09-16 00:32:11 -0400

    add update and delete functionality

--- app.rb
+++ app.rb
@@ -15,28 +15,10 @@ enable :sessions
 # configure ActiveRecord: connect to postgres
 ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/be_scott')
 
-# configure ActiveRecord: connect to postgres
-# ActiveRecord::Base.establish_connection(
-#   :adapter => "postgresql",
-#   :host => "localhost",
-#   :username => "scottsMac", # Put your postgres username here
-#   :password => "",
-#   :database => "be_scott"
-# )
-
 # output ActiveRecord sql to console
 ActiveRecord::Base.logger = Logger.new(STDOUT)
 
-# buttons = {
-# 	not_logged_in: [
-# 		{text: "become a Scott", url: "/signup"},
-# 		{text: "log in (as Scott)", url: "/login"}
-# 	],
-# 	logged_in: [
-# 		{text: "acquire a mad skill", url: "/scott/new_mad_skill"},
-# 		{text: "acquire an eclectic instrument", url: "/scott/new_eclectic_instrument"}
-# 	]
-# }
+
 
 before do
 	if session[:scott]
@@ -110,29 +92,109 @@ get "/scott" do
 	erb :show
 end
 
+
+
 ## NEW mad skill
 get "/scott/new_mad_skill" do
 	@skill_genres = $CONFIG[:skill_genres]
+	@post_path = "/new_mad_skill"
 	erb :new_mad_skill
 end
 
 post "/scott/new_mad_skill" do
 	skill = @scott.mad_skills.new params[:skill]
 	skill.save
+
 	redirect "/scott"
 end
+
+
+
+## EDIT mad skill
+get "/scott/edit_mad_skill/:skill_id" do
+	begin
+		@mad_skill = @scott.mad_skills.find params[:skill_id]
+		@post_path = "/edit_mad_skill/#{@mad_skill.id}"
+	rescue 
+		redirect "/scott"
+	end
+	@skill_genres = $CONFIG[:skill_genres]
+	erb :new_mad_skill
+end
+
+post "/scott/edit_mad_skill/:skill_id" do
+	begin
+		ms = @scott.mad_skills.find params[:skill_id]
+		ms.update_attributes params[:skill]
+	rescue 
+	end
+	redirect "/scott"
+end
+
+## DELETE mad skill
+get "/scott/delete_mad_skill/:skill_id" do
+	begin
+		ms = @scott.mad_skills.find params[:skill_id]
+		ms.destroy
+	rescue 
+	end
+	redirect "/scott"
+end
+
+
 
 ## NEW eclectic instrument
 get "/scott/new_eclectic_instrument" do
 	@instrument_genres = $CONFIG[:instrument_genres]
+	@post_path = "/new_eclectic_instrument"
 	erb :new_eclectic_instrument
 end
 
 post "/scott/new_eclectic_instrument" do
 	ei = @scott.eclectic_instruments.new params[:ei]
 	ei.save
+
 	redirect "/scott"
 end
+
+
+
+## EDIT eclectic instrument
+get "/scott/edit_eclectic_instrument/:instrument_id" do
+	begin
+		@eclectic_instrument = @scott.eclectic_instruments.find params[:instrument_id]
+		@post_path = "/edit_eclectic_instrument/#{@eclectic_instrument.id}"
+		# session[:ei_id] = @eclectic_instrument.id
+	rescue
+		redirect "/scott"
+	end
+
+	@instrument_genres = $CONFIG[:instrument_genres]
+	erb :new_eclectic_instrument
+end
+
+post "/scott/edit_eclectic_instrument/:instrument_id" do
+	begin
+		ei = @scott.eclectic_instruments.find params[:instrument_id]
+		ei.update_attributes params[:ei]
+		session[:ei_id] = nil
+	rescue
+	end
+
+	redirect "/scott"
+end
+
+
+## DELETE mad skill
+get "/scott/delete_eclectic_instrument/:instrument_id" do
+	begin
+		eclectic_instrument = @scott.eclectic_instruments.find params[:instrument_id]
+		eclectic_instrument.destroy
+	rescue 
+	end
+	redirect "/scott"
+end
+
 
 
 
